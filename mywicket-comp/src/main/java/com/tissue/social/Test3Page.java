5a52f6dc50ce1b7dcbd4b05eee91d71d70482ddf
guoyingshou <guoyingshou@gmail.com>
2014-06-22 18:06:25 +0800

    added templating with markup inheritance

---
+++ mywicket-comp/src/main/java/com/tissue/social/Test3Page.java
@@ -0,0 +1,21 @@
+package com.tissue.social;
+
+import com.tissue.social.web.templates.MyTemplateWithMarkupInheritance;
+
+public class Test3Page extends MyTemplateWithMarkupInheritance {
+
+    private static final long serialVersionUID = 1l;
+
+    public Test3Page() {
+        super();
+        System.out.println("test3page noooo param...");
+    }
+
+    /**
+    public Test2Page(PageParameters param) {
+        System.out.println("param...");
+
+    }
+    */
+
+}
